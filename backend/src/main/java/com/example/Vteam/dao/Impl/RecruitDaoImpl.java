f00ae47346aaf8a9f6f89b94a2215489532773cb
Langwenchong <1422257646@qq.com>
2022-05-09 15:16:43 +0800

    招募创建

---
+++ backend/src/main/java/com/example/Vteam/dao/Impl/RecruitDaoImpl.java
@@ -0,0 +1,36 @@
+package com.example.Vteam.dao.Impl;
+
+import com.example.Vteam.dao.Interface.RecruitDao;
+import com.example.Vteam.entity.RecruitInfo;
+import com.example.Vteam.repository.RecruitInfoRepository;
+import com.example.Vteam.utils.MyFunction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+/**
+ * @Author Lang wenchong
+ * @Date 2022/5/9 14:36
+ * @Version 1.0
+ */
+public class RecruitDaoImpl implements RecruitDao {
+
+    @Autowired
+    RecruitInfoRepository recruitInfoRepository;
+
+    @Override
+    public RecruitInfo createRecruitInfo() {
+        String rid = UUID.randomUUID().toString().replaceAll("-", "");
+        return new RecruitInfo(rid, "", "", 0, "", "", "", "", "", MyFunction.getTime(), "", "", 0, 0);
+    }
+
+    @Override
+    public int insertRecruitInfo(RecruitInfo recruitInfo) {
+        if (!recruitInfoRepository.existsById(recruitInfo.getRid())) {
+            recruitInfoRepository.save(recruitInfo);
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
