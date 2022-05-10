335d91f0afa8009757dacb4a79cfb7ddde6b0588
‘huangshuo’ <1625744287@qq.com>
2022-05-10 10:13:13 +0800

    注册接口

--- frontend/user/src/apis/welcome.js
+++ frontend/user/src/apis/welcome.js
@@ -7,3 +7,16 @@ export const login = (username, password) => {
   fd.append("password", password);
   return axios.post("/user/api/login", fd);
 }
+
+export const register = (ruleForm,avator,username,email,password) => {
+  let fd = new FormData();
+  fd.append("name",ruleForm.name);
+  fd.append("phoneNumber",ruleForm.phoneNumber);
+  fd.append("school",ruleForm.school);
+  fd.append("sex",ruleForm.sex);
+  fd.append("avator",avator);
+  fd.append("username",username);
+  fd.append("email",email);
+  fd.append("password",password);
+  return axios.post('user/api/register',fd);
+}
